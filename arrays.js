394d1aa2cc5048210a735cd6707f13b1c73688e4
Maria Nunez <mariaalician@gmail.com>
2018-05-13 16:03:40 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+var destructivelyAddElementToBeginningOfArray = ["milky way"];
+
+//var addElementToEndOfArray = [];//
+
+//var destructivelyAddElementToEndOfArray = [];//
+
+//var accessElementInArray = []//
+
+//var destructivelyRemoveElementFromBeginningOfArray = [];//
